83bddf40bcea6c13cb79a0c619a1c8c9b5ded9eb
vantrong10e1 <email.tranvantrong.2014@gmail.com>
2026-05-18 19:21:44 +0900

    Thêm màn hình login

---
+++ Quan-Ly-Kho-Hang/Controllers/ForgetUsernameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForgetUsernameViewController.swift
+//  Quan-Ly-Kho-Hang
+//
+//  Created by vantrong10e1 on 2026/05/18.
+//
+
+import UIKit
+
+class ForgetUsernameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
