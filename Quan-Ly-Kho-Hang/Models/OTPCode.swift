85e7336ff5ecb9d4aca4078b520bc46475cbbf26
vantrong10e1 <email.tranvantrong.2014@gmail.com>
2026-05-30 11:25:14 +0900

    Viet tiep ky thuat Swift + Firebase trong UI

--- Quan-Ly-Kho-Hang/Models/OTPCode.swift
+++ Quan-Ly-Kho-Hang/Models/OTPCode.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class OTPCode {
+    private var _email: String
+    private var _otp: Int
+    private var _createdAt: Timestamp
+    
+    init(email: String, otp: Int, createdAt: Timestamp) {
+        self._email = email
+        self._otp = otp
+        self._createdAt = createdAt
+    }
+    
+    public var email: String {
+        get {
+            return _email
+        }
+        set {
+            _email = newValue
+        }
+    }
+    
+    public var otp: Int {
+        get {
+            return _otp
+        }
+        set {
+            _otp = newValue
+        }
+    }
+    
+    public var createdAt: Timestamp {
+        get {
+            return _createdAt
+        }
+        set {
+            _createdAt = newValue
+        }
+    }
+}
+
